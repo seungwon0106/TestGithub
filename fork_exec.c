ef8e37f5d5fe432841c1368b375a99e9cccc3f80
seungwon0106 <48343593+seungwon0106@users.noreply.github.com>
2019-03-15 16:39:38 +0900

    fork() - myexecv
    ls -al 처럼 execv이용하여 경로 찾기

---
+++ fork_exec.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+
+void my_execv(const char *path, char *const argv[])
+{
+	int ret;
+	pid_t pid;
+
+	pid = fork();
+
+	if(pid ==-1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(pid ==0)
+	{
+		ret = execv(path, argv);
+		if(ret ==-1)
+		{
+			perror("execv");
+			exit(EXIT_FAILURE);
+		
+		}
+
+	}
+	wait(NULL);
+}
+
+
+int main ()
+{
+	char path[] = "/bin/ls";
+	char *argv_ls[] = {"ls", "-al", NULL};
+
+	my_execv(path, argv_ls);
+
+	return 0;
+
+}
